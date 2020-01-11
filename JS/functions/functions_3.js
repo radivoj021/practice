32c7b52b7b89765199b2a8913bac88f60e7f6120
radivoj021 <rapic.021@gmail.com>
2020-01-11 19:07:28 +0100

    f3-8

--- JS/functions/functions_3.js
+++ JS/functions/functions_3.js
@@ -98,3 +98,102 @@ for (var i = niz.length - 1; i >= 0; i--) {
 
 console.log(array);
  */
+
+/* 6. Write a function to create a specified number of elements with pre-filled numeric value
+array.
+6, 0 -> [0, 0, 0, 0, 0, 0]
+2, "none" -> ["none", "none"]
+2 -> [null, null] */
+
+/* function array(n, val) {
+  var niz = [];
+  for (var i = 0; i < n; i++) {
+    if (val === undefined) {
+      val = null;
+      niz.push(val);
+    } else {
+      niz.push(val);
+    }
+  }
+  return niz;
+}
+
+var array = array(3);
+console.log(array); */
+
+/* 7. Write a function that says whether a number is perfect.
+28 -> 28 is a perfect number.
+Note: According to Wikipedia: In number theory, a perfect number is a positive integer that is equal to the sum
+of its proper positive divisors, that is, the sum of its positive divisors excluding the number itself (also known
+as its aliquot sum). Equivalently, a perfect number is a number that is half the sum of all of its positive divisors
+(including itself). */
+/* 
+function sum(n) {
+  var res = 0;
+  for (var i = 0; i <= n; i++) {
+    if (n % i === 0) {
+      res += i;
+    }
+  }
+  console.log(n);
+  if (res / 2 === n) {
+    console.log("Number is perfect");
+  } else {
+    console.log("Number is not");
+  }
+}
+
+sum(496);
+ */
+
+/* 8. Write a function to find a word within a string.
+'The quick brown fox', 'fox' -> "'fox' was found 1 times"
+' a, bb, cc, dd, aa', 'aa' -> "'aa' was found 2 times" */
+
+/* function search(text, word) {
+  var x = 0,
+    y = 0;
+
+  for (var i = 0; i < text.length; i++) {
+    if (text[i] == word[0]) {
+      for (j = i; j < i + word.length; j++) {
+        if (text[j] == word[j - i]) {
+          y++;
+        }
+        if (y == word.length) {
+          x++;
+        }
+      }
+      y = 0;
+    }
+  }
+  return word + " was found " + x + " times.";
+}
+
+var txt = "this is fox is this fox";
+var l = "fox";
+var search = search(txt, l);
+console.log(search);
+ */
+
+var txt = "Big fox is faster than a small fox";
+var wrd = "fox";
+
+var y = 0;
+
+function count(text, word) {
+  var res = "";
+  for (var i = 0; i < text.length; i++) {
+    if (text[i] == word[0]) {
+      for (var j = i; j < word.length + i; j++) {
+        if (text[j] == word[j - i]) {
+          y++;
+        }
+      }
+    }
+  }
+  return y;
+}
+
+var count = count(txt, wrd);
+console.log(count);
